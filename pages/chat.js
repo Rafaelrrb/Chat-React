5075640920d764b1e9938438b1518847df84ff37
Rafael Borba <rafaeborba@gmail.com>
2022-01-28 01:37:50 -0300

    overflou: auto

--- pages/chat.js
+++ pages/chat.js
@@ -159,7 +159,7 @@ function MessageList(props) {
     <Box
       tag="ul"
       styleSheet={{
-        overflow: 'scroll',
+        overflow: 'auto',
         display: 'flex',
         flexDirection: 'column-reverse',
         flex: 1,
